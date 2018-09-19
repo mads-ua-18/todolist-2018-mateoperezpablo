948c312d77d6e9c398ba7cf7f81493af24a90d8f
Pablo Mateo Pérez <mateoperez.pablo@gmail.com>
2018-09-19 14:04:25 +0200

    Barra de navegación con enlace ToDoList y taraeas funciona correctamente

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,18 @@
 package controllers;
 
-import play.mvc.*;
+import models.Usuario;
+import play.Logger;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security;
+import security.ActionAuthenticator;
+import services.UsuarioService;
 
 import views.html.*;
+
+import javax.inject.Inject;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -10,6 +20,8 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
+    @Inject
+    UsuarioService usuarioService;
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
@@ -21,7 +33,15 @@ public class HomeController extends Controller {
     }
 
     public Result about() {
-        return ok(about.render());
+        String connectedUserStr = session("connected");
+        Long connectedUser = Long.valueOf(connectedUserStr);
+        Usuario usuario = usuarioService.findUsuarioPorId(connectedUser);
+        if (usuario == null) {
+            return ok(about.render(null));
+        } else {
+            Logger.debug("Encontrado usuario para about " + usuario.getId() + ": " + usuario.getLogin());
+            return ok(about.render(usuario));
+        }
     }
 
 }
